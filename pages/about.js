20caee8db1852540cc57fcf4d8db269161f55ba0
Eduardo Moreno <emoreno911@gmail.com>
2020-11-22 11:25:37 -0400

    Add About page

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import styles from '../styles/Category.module.css'
+
+ const About = () => {
+  return (
+    <Layout>
+			<Header />
+			<section>
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis. Sagittis aliquam malesuada bibendum arcu vitae. Magna fermentum iaculis eu non. Purus sit amet volutpat consequat. Mattis rhoncus urna neque viverra. 
+				</p>
+				<p>
+				Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. Et malesuada fames ac turpis egestas. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis.
+				</p>
+			</section>
+		</Layout>
+  )
+}
+
+export default About

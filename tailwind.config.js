de04e6c70d004b1c93f75ac13dc5cc1f39cf694d
Eduardo Moreno <emoreno911@gmail.com>
2020-11-23 21:58:19 -0400

    quick fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./pages/**/*.js', './components/**/*.js']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
